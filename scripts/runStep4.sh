9ee63b19b02937c2a1f45a1d104493bb8dd4b8f1
James MacDonald <jmacdon@pearson2.gcc.biostat.washington.edu>
2021-08-04 11:36:38 -0700

    Changed the #SNPs between breakpoints for step 4 to a variable rather than being hard-coded.

--- scripts/runStep4.sh
+++ scripts/runStep4.sh
@@ -9,6 +9,7 @@
 ## to allow for superpopulation specific analysis we
 ## added a pop subdir
 pop=$1
+nsnps=$2
 
 source env/bin/activate
 
@@ -16,5 +17,5 @@ python3 P02_minima_pipeline.py \
 	--input_fname="$pop"/chr"$SGE_TASK_ID".vector.txt.gz \
 	--chr_name=chr"$SGE_TASK_ID" \
 	--dataset_path="$pop"/ \
-	--n_snps_bw_bpoints=50 \
+	--n_snps_bw_bpoints="$nsnps" \
 	--out_fname="$pop"/chr"$SGE_TASK_ID"_minima.pickle
